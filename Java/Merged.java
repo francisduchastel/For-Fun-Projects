9d449ca9a81e40298bf05334ea20e8fa1b3213dc
francisduchastel <119887498+francisduchastel@users.noreply.github.com>
2024-04-19 11:48:31 -0400

    Add files via upload

---
+++ Java/Merged.java
@@ -0,0 +1,166 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Merged {
+    public static void main(String[] args) {
+        
+        Scanner Input = new Scanner(System.in);
+
+        System.out.println("What is the name of the .csv file you want converted?");
+
+        String Map_filepath = "";
+        Map_filepath += Input.nextLine();
+        Map_filepath += ".csv";
+
+
+        String Map_Name = "";
+
+        System.out.println("What Should the Created File be called?");
+
+        Map_Name += Input.nextLine() + ".txt";
+
+        Input.close();
+
+
+        String Map_Data = "";
+
+        String Converted_Info = "";
+
+        int Block_x = 0;
+        int Block_y = 0;
+        
+
+
+        try {
+            // try to open the file and extract its contents
+            Scanner scn = new Scanner(new File(Map_filepath));
+            while (scn.hasNextLine()) {
+            String line = scn.nextLine();
+            Map_Data += line + "\n"; // nextLine() removes line breaks, so add them back in
+            }
+            scn.close(); // be nice and close the Scanner
+        }
+        catch (FileNotFoundException e) {
+        }
+
+        Boolean[][] Grid = new Boolean[1000][1000];
+
+        for (int a = 0; a < 1000; a++) {
+            for (int b = 0; b < 1000; b++) {
+
+                Grid[a][b] = false;
+
+            }
+        }
+        
+        
+
+
+
+        String[] Lines = Map_Data.split("\n");
+
+        for (int i = 0; i < Lines.length; i++) {
+            
+
+            int x_coord = 0;
+
+            for (int p = 0; p < Lines[i].length(); p++) {
+
+                if (Lines[i].charAt(p) == ',') {
+
+                    x_coord++;
+
+
+                } else {
+
+                    if (Lines[i].charAt(p) == 'A') {
+
+                        Grid[x_coord][i] = true;
+
+                    }
+
+
+                    if (Lines[i].charAt(p) == 'B') {
+
+
+                        Block_x = x_coord * 20;
+                        Block_y = i * 20;
+
+                        Converted_Info = "" + Block_x +", " + Block_y + ",\n";
+
+
+                    }
+
+
+                }
+
+            }
+
+
+
+
+        }
+
+
+        
+
+        for (int z = 0; z < 1000; z++) {
+
+            for (int y = 0; y < 1000; y++) {
+
+                int height = 0;
+                
+                if (Grid[z][y] == true) {
+
+                    
+
+                    height++;
+
+                    while (Grid[z][y + height] == true) {
+
+                        Grid[z][y + height] = false;
+
+                        height++;
+
+                    }
+
+
+                    Block_x = z * 20;
+                    Block_y = y * 20;
+
+
+                    Converted_Info += "" + (Block_x + 1) + ", " + Block_y + ", 18, 10, 1,\n";
+
+                    Converted_Info += "" + (Block_x + 1) + ", " + (Block_y + (height * 20)) + ", 18, 10, 3,\n";
+
+                    Converted_Info += "" + (Block_x + 1) + ", " + (Block_y + 5) + ", 18, " + (-5 + (height * 20))  +", 2,\n"; 
+
+
+
+                }
+
+            }
+
+
+            
+
+        }
+
+
+
+        try {
+            FileWriter myWriter = new FileWriter(Map_Name);
+            myWriter.write(Converted_Info);
+            myWriter.close();
+        } catch (IOException e) {}
+
+
+
+
+
+    }
+}
